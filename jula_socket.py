7af6eba043807dd32bf8e6dbb9f7cabee76901e7
root <root@raspberrypi.(none)>
2015-04-01 21:14:25 +0000

    made socket script autostart when connection is lost

--- jula_socket.py
+++ jula_socket.py
@@ -2,19 +2,18 @@ from ws4py.client.threadedclient import WebSocketClient
 import jula
 import json
 import time
+import sys
 
 class DummyClient(WebSocketClient):
     def opened(self):
-        def data_provider():
-            for i in range(1, 200, 25):
-                yield "#" * i
+        #def data_provider():
+        #    for i in range(1, 200, 25):
+        #        yield "#" * i
 
-        #self.send(data_provider())
-
-        for i in range(0, 200, 25):
-            print i
+        # for i in range(0, 200, 25):
+        #    print i
             #self.send("*" * i)
-
+	print 'connected'
     def closed(self, code, reason=None):
         print "Closed down", code, reason
 
@@ -30,11 +29,16 @@ class DummyClient(WebSocketClient):
 		print "got off"
 
 if __name__ == '__main__':
-    try:
-	name = "pi-" +str(time.time())
-        ws = DummyClient('ws://infinite-refuge-5280.herokuapp.com/room/chat?username=' +name)
-        ws.connect()
-        ws.run_forever()
-    except KeyboardInterrupt:
-        ws.close()
+	while True:    
+		print 'connecting'
+		try:
+			name = "pi-" +str(time.time())
+        		ws = DummyClient('ws://infinite-refuge-5280.herokuapp.com/room/chat?username=' +name)
+        		ws.connect()
+        		ws.run_forever()
+    		except KeyboardInterrupt:
+        		ws.close()
+			sys.exit()
+		print 'lost connection'
+	print " Script end"
 
